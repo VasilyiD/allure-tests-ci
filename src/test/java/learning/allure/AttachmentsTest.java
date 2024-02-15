5858b4a9e0b279c0c589ada7db807c128635fc96
VasilyiD <revodvv@mail.ru>
2024-02-15 22:17:09 +0300

    Allure-tests + CI

--- src/test/java/learning/allure/AttachmentsTest.java
+++ src/test/java/learning/allure/AttachmentsTest.java
@@ -1,54 +1,33 @@
 package learning.allure;
-
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
 
-public class StepTest {
+public class AttachmentsTest {
 
     private static final String REPOSITORY = "eroshenkoam/allure-example";
     private static final String ISSUE = "87";
 
     @Test
-    public void testLambdaStep() {
+    public void testLambdaAttachments() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         step("Открываем главную страницу", () -> {
             open("https://github.com/");
-        });
-        step("Ищем репозиторий " + REPOSITORY, () -> {
-            $("[data-target= 'qbsearch-input.inputButtonText']").click();
-            $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
-            $("#query-builder-test").submit();
-        });
-        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
-            $("[href ='/eroshenkoam/allure-example']").click();
-        });
-        step("Открываем таб Issuee", () -> {
-            $("#issues-tab").click();
-        });
-        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
-            $(withText("#" + ISSUE)).should(Condition.exist);
+            attachment("Source", webdriver().driver().source());
         });
 
     }
         @Test
-        public void testAnnotatedStep() {
+        public void testAnnotatedAttachments() {
             SelenideLogger.addListener("allure", new AllureSelenide());
             WebSteps steps = new WebSteps();
 
             steps.openMainPage();
-            steps.searchForRepository(REPOSITORY);
-            steps.clickOnRepositoryLink(REPOSITORY);
-            steps.openIssuesTab();
-            steps.shouldSeeIssueWithNumber(ISSUE);
+            steps.takeScreenshot();
         }
 
 
--- src/test/java/learning/allure/LabelsTest.java
+++ src/test/java/learning/allure/LabelsTest.java
@@ -1,46 +1,29 @@
 package learning.allure;
-
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.attachment;
-import static io.qameta.allure.Allure.step;
-
-public class AttachmentsTest {
-
-    private static final String REPOSITORY = "eroshenkoam/allure-example";
-    private static final String ISSUE = "87";
+public class LabelsTest {
 
     @Test
-    public void testLambdaAttachments() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Открываем главную страницу", () -> {
-            open("https://github.com/");
-            attachment("Source", webdriver().driver().source());
-        });
-
-    }
-        @Test
-        public void testAnnotatedAttachments() {
-            SelenideLogger.addListener("allure", new AllureSelenide());
-            WebSteps steps = new WebSteps();
-
-            steps.openMainPage();
-            steps.takeScreenshot();
-        }
-
-
-
- /*  step("Что-то делаем", new Allure.ThrowableContextRunnableVoid<Allure.StepContext>() {
-                @Override
-                public void run(Allure.StepContext stepContext) throws Throwable {
-
-                }
-            });*/
+    @Feature("Issue в репозитории")
+    @Story("Создание Issue")
+    @Owner("eroshenkoam")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Testing", url = "https://github.com/")
+    @DisplayName("Создание Issue для авторизованного пользователя")
+    public void testStaticLabels() {
 
     }
+
+    @Test
+    public void testDynamicLabels() {
+        Allure.getLifecycle().updateTestCase(
+                t -> t.setName("Создание Issue для авторизованного пользователя")
+        );
+        Allure.feature("Issue в репозитории");
+        Allure.story("Создание Issue");
+        Allure.label("owner","eroshenkoam");
+        Allure.label("severity",SeverityLevel.BLOCKER.value());
+        Allure.link("testing","https://github.com/");
+    }
+}
